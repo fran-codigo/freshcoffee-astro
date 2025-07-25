1133ccb596c589908dfe0179a9b146bef5451341
Franco <franco_tigres18@hotmail.com>
2025-07-24 21:55:51 -0600

    feat(quiosco_products):show product from the api and types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,19 @@
 import { z } from 'astro:content';
 
+const ImageSchema = z.object({
+  url: z.string().url(),
+  width: z.number(),
+  height: z.number(),
+});
+
+const FeaturedImageSchema = z.object({
+  thumbnail: ImageSchema,
+  medium: ImageSchema,
+  medium_large: ImageSchema,
+  large: ImageSchema,
+  full: ImageSchema,
+});
+
 const categorySchema = z.object({
   id: z.number(),
   name: z.string(),
@@ -10,5 +24,17 @@ const categorySchema = z.object({
 });
 
 export const CategoriesSchema = z.array(categorySchema);
-
 export type Category = z.infer<typeof categorySchema>;
+
+const productSchema = z.object({
+  id: z.number(),
+  slug: z.string(),
+  title: z.object({
+    rendered: z.string(),
+  }),
+  featured_media: z.number(),
+  featured_images: FeaturedImageSchema,
+});
+
+export const ProductsSchema = z.array(productSchema);
+export type Product = z.infer<typeof productSchema>;
